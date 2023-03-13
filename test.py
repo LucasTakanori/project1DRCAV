dd4d1fc25b635a99a070ec74a1b7a34733ac98ef
LucasTakanori <79314835+LucasTakanori@users.noreply.github.com>
2023-03-13 15:59:19 +0100

    dataset csd descriptor implementation

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 import subprocess,os
+import os
+
+def find_images(folder_path, output_file):
+    with open(output_file, "w") as f:
+        for dirpath, dirnames, filenames in os.walk(folder_path):
+            for filename in filenames:
+                if filename.lower().endswith((".jpg", ".jpeg", ".png", ".gif")):
+                    f.write(os.path.join(dirpath, filename) + "\n")
+
 
 
 #os.system("cd DATABASE")
@@ -18,6 +27,12 @@ text_file.write(str)
 #close file
 text_file.close()
 
+
+# Example usage
+#folder_path = "/path/to/folder"
+#output_file = "/path/to/output.txt"
+find_images(path, path+"\mpeg7fex_win32_v2"+"\imgtest1.txt")
+
 os.chdir(path+"\mpeg7fex_win32_v2")
 
-os.system("MPEG7Fex.exe CSD 64 imgtest.txt CSD1.txt")
+os.system("MPEG7Fex.exe CSD 64 imgtest1.txt CSD12.txt")
