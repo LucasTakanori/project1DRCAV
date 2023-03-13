3059525c51b9ca681c2377016691c3558de38fc5
LucasTakanori <79314835+LucasTakanori@users.noreply.github.com>
2023-03-13 16:55:36 +0100

    optimized functions

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 import subprocess,os
 import os
 
+dbimglist = "db_img_list.txt"
+mpegpth="\mpeg7fex_win32_v2"
+db="\DATABASE"
+outputDescriptor="CSDDB.txt"
+featureType = "CSD" 
+featureParameters = 64
+
 def find_images(folder_path, output_file):
     with open(output_file, "w") as f:
         for dirpath, dirnames, filenames in os.walk(folder_path):
@@ -9,30 +16,20 @@ def find_images(folder_path, output_file):
                     f.write(os.path.join(dirpath, filename) + "\n")
 
 
+def calculate_Descriptor(path, pathScript, pathFiles, featureType, featureParameters, imglist, outputDescriptor):
 
+    find_images(path+db, path+db+ "\\" + imglist)
+
+    os.chdir(path+pathScript)
+
+    os.system("MPEG7Fex.exe " + featureType + " " + str(featureParameters) + " " + imglist + " " + outputDescriptor)
 #os.system("cd DATABASE")
-path = os.getcwd()
-mylist = os.listdir(path+"\DATABASE")
-print(path)
-str=("\n"+path+"\DATABASE\\").join(mylist)
-str=path+"\DATABASE\\"+str
-#open text file
-imgpath=path+"\mpeg7fex_win32_v2\imgtest.txt"
-print(imgpath)
-text_file = open(imgpath, "w")
- 
-#write string to file
-text_file.write(str)
- 
-#close file
-text_file.close()
+
+
 
 
 # Example usage
 #folder_path = "/path/to/folder"
 #output_file = "/path/to/output.txt"
-find_images(path, path+"\mpeg7fex_win32_v2"+"\imgtest1.txt")
-
-os.chdir(path+"\mpeg7fex_win32_v2")
-
-os.system("MPEG7Fex.exe CSD 64 imgtest1.txt CSD12.txt")
+path = os.getcwd()
+calculate_Descriptor(path, mpegpth, db, featureType, featureParameters, dbimglist, outputDescriptor)
