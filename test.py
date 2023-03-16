f4358183feb2fda7e35a2756b5eed215509d44ae
LucasTakanori <79314835+LucasTakanori@users.noreply.github.com>
2023-03-16 09:20:44 +0100

    added query csd calculation

--- test.py
+++ test.py
@@ -5,11 +5,12 @@ import os
 dbimglist = "db_img_list.txt"
 mpegpth = "\mpeg7fex_win32_v2"
 db = "\DATABASE"
-outputDescriptor = "CSDDB.txt"
+outputDescriptorDb = "CSDDB.txt"
+outputDescriptorQry = "CSDqry.txt"
 featureType = "CSD"
 featureParameters = 64
-
-
+querypth = "\QUERY"
+qrylist = "query_list.txt"
 def find_images(folder_path, output_file):
     with open(output_file, "w") as f:
         for dirpath, dirnames, filenames in os.walk(folder_path):
@@ -20,14 +21,19 @@ def find_images(folder_path, output_file):
 
 def calculate_Descriptor(path, pathScript, pathFiles, featureType, featureParameters, imglist, outputDescriptor):
 
-    find_images(path+pathFiles, path+pathFiles + "\\" + imglist)
+    find_images(path+pathFiles, path+pathScript + "\\" + imglist)
 
     os.chdir(path+pathScript)
 
     os.system("MPEG7Fex.exe " + featureType + " " +
               str(featureParameters) + " " + imglist + " " + outputDescriptor)
+    os.chdir(path)
 
 
 path = os.getcwd()
 calculate_Descriptor(path, mpegpth, db, featureType,
-                     featureParameters, dbimglist, outputDescriptor)
+                     featureParameters, dbimglist, outputDescriptorDb)
+
+calculate_Descriptor(path, mpegpth, querypth, featureType,
+                     featureParameters, qrylist, outputDescriptorQry)
+
